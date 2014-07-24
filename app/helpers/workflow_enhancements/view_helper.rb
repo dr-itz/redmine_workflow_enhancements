b727a4bc7c69bdd79c8941feb9bd718a31d41642
Daniel Ritz <daniel.ritz@gmx.ch>
2014-07-24 02:11:59 +0200

    Cleanups, visual updates

--- app/helpers/workflow_enhancements/view_helper.rb
+++ app/helpers/workflow_enhancements/view_helper.rb
@@ -1,16 +1,15 @@
 module WorkflowEnhancements::ViewHelper
 
-	def render_super(current_path)
-		details = lookup_context.registered_details.inject({}) do |h, n|
-			h[n] = lookup_context.send(n); h
-		end
-		details[:locale] = [details[:locale]] unless details[:locale].is_a? Array
+  def render_super(current_path)
+    details = lookup_context.registered_details.inject({}) do |h, n|
+      h[n] = lookup_context.send(n); h
+    end
+    details[:locale] = [details[:locale]] unless details[:locale].is_a? Array
 
-		parent_view = view_paths.map do |path|
-			path.find_all(File.basename(current_path).split('.').first,
-         lookup_context.prefixes.first, false, details).first.try(:identifier)
-		end.compact.drop_while {|path| path != current_path}[1].split('.').first
-		render :file => parent_view
+    parent_views = view_paths.map do |path|
+      path.find_all(File.basename(current_path).split('.').first,
+        lookup_context.prefixes.first, false, details).first.try(:identifier)
+    end.compact.delete_if {|path| !path || path == current_path}
+    render :file => parent_views.first.split('.').first
   end
-
 end
