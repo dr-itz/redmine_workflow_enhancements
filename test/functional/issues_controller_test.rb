c2c6b52c8d30fbcc784a174c420ff6748d631e76
Daniel Ritz <daniel.ritz@gmx.ch>
2014-08-12 00:15:28 +0200

    Add some tests for the issue dialog
    This unfortunately copies a lot of fixtures...

---
+++ test/functional/issues_controller_test.rb
@@ -0,0 +1,38 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class IssuesControllerTest < ActionController::TestCase
+  fixtures :projects,
+           :users,
+           :roles,
+           :members,
+           :member_roles,
+           :issues,
+           :issue_statuses,
+           :issue_categories,
+           :trackers,
+           :projects_trackers,
+           :enabled_modules,
+           :enumerations,
+           :workflows
+
+  def setup
+    User.current = nil
+    @request.session[:user_id] = 2 # manager
+  end
+
+  def test_show_by_manager
+    get :show, :id => 1
+    assert_response :success
+    assert_template 'show'
+
+    assert_equal Issue.find(1), assigns(:issue)
+
+    # test if standard Redmine stuff is there (i.e. render_super() works)
+    assert_select 'a', :text => /Quote/
+    assert_select 'form#issue-form', true
+    assert_select '#issue_tracker_id', true
+
+    # test if workflow visualization is there
+    assert_select '#workflow-display', true
+  end
+end
---
+++ test/functional/workflow_enhancements_controller_test.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class WorkflowEnhancementsControllerTest < ActionController::TestCase
+  fixtures :projects,
+           :users,
+           :roles,
+           :members,
+           :member_roles,
+           :issues,
+           :issue_statuses,
+           :issue_categories,
+           :trackers,
+           :projects_trackers,
+           :enabled_modules,
+           :enumerations,
+           :workflows
+
+  def setup
+    User.current = nil
+    @request.session[:user_id] = 2 # manager
+  end
+
+  def test_show_as_manager_newissue
+    get :show, :project_id => 1, :tracker_id => 1, :issue_id => 0
+    assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:project)
+    assert_not_nil assigns(:roles)
+    assert_not_nil assigns(:tracker)
+
+    # test if workflow visualization is there
+    assert_select 'svg#workflow-vis', true
+    assert_select 'graphContextual', false
+  end
+end
