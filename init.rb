fd4d485b455f73ba2d296b39e035f3319edbead3
Daniel Ritz <daniel.ritz@gmx.ch>
2015-04-03 21:52:47 +0200

    Version v0.4.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_workflow_enhancements do
   name 'Redmine Workflow Enhancements'
   author 'Daniel Ritz'
   description 'Enhancements for Workflow'
-  version '0.3.0'
+  version '0.4.0'
   url 'https://github.com/dr-itz/redmine_workflow_enhancements'
   author_url 'https://github.com/dr-itz/'
 
